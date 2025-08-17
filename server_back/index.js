29cd618ffdea3b0d4d0ec0d7142f7613a26a49f9
Saurabh Singh <singh.saurabh2250@gmail.com>
2025-08-18 00:40:31 +0530

    resolved webhook

--- server_back/index.js
+++ server_back/index.js
@@ -12,8 +12,8 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin:"https://cognito-zots.onrender.com/",
+    credentials:true    
 }));
 
 
